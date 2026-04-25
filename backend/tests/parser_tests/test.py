495f9843c8121154fcf880215e7666fd117cd21b
Nenad Skokovic <nenad.skokovic@gmail.com>
2026-04-25 18:31:01 +0200

    Updated bonus

--- backend/tests/parser_tests/test.py
+++ backend/tests/parser_tests/test.py
@@ -1,31 +1,34 @@
-# import sys
-# sys.path.insert(0, "/home/nenad/work/hackupc/hackupc/backend")
-
-# import json
-# from parser import parse_repo
-
-# result = parse_repo("parser_tests/katana-swift", repo_name="BendingSpoons/katana-swift")
-
-# with open("parser_tests/output.json", "w") as f:
-#     json.dump(result, f, indent=2)
-
-# print("Done! Check test/output.json")
-
-# test.py
 import sys
 sys.path.insert(0, "/home/nenad/work/hackupc/hackupc/backend")
 
 import json
 from parser import parse_repo
 
-test_folders = ["test1", "test2", "test3"]
+result = parse_repo("parser_tests/katana-swift", repo_name="BendingSpoons/katana-swift")
 
-for folder in test_folders:
-    print(f"Parsing {folder}...")
-    result = parse_repo(folder, repo_name=folder)
+with open("parser_tests/output.json", "w") as f:
+    json.dump(result, f, indent=2)
+
+print("Done! Check test/output.json")
+
+
+
+
+# test.py
+# import sys
+# sys.path.insert(0, "/home/nenad/work/hackupc/hackupc/backend")
+
+# import json
+# from parser import parse_repo
+
+# test_folders = ["test1", "test2", "test3"]
+
+# for folder in test_folders:
+#     print(f"Parsing {folder}...")
+#     result = parse_repo(folder, repo_name=folder)
     
-    output_file = f"{folder}_output.json"
-    with open(output_file, "w") as f:
-        json.dump(result, f, indent=2)
+#     output_file = f"{folder}_output.json"
+#     with open(output_file, "w") as f:
+#         json.dump(result, f, indent=2)
     
-    print(f"Done! Check {output_file}")
+#     print(f"Done! Check {output_file}")
